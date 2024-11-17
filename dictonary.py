43f17bd103cf0e159131a2ee5994a9d37ea91c96
NabilaTanzim <nabila.tahwa@gmail.com>
2024-11-17 23:12:30 +0600

    Adding code

--- dictonary.py
+++ dictonary.py
@@ -1,1 +1,13 @@
-#I did nothing
+student = {
+    "Nabila" : {
+        "Roll"  : "011",
+        "Department" : "RME",
+        "Faculty" : "Engineering and Technology"
+        },
+"Tahwa" : {
+        "Roll"  : "050",
+        "Department" : "RME",
+        "Faculty" : "Engineering and Technology"
+        }
+}
+print(student["Nabila"]["Department"])
