eece7ac6a83ac400981a11e3055f2641d4d232c5
NabilaTanzim <nabila.tahwa@gmail.com>
2025-02-15 15:43:30 +0600

    everything before oop

--- fileeeee.py
+++ fileeeee.py
@@ -1,24 +1,27 @@
-x = open('newfile.txt', 'r')
-print(x.read())
+# x = open('newfile.txt', 'r')
+# print(x.read())
+#
+# y = open("anotherfile.txt", "w")
+# y.write("gibberish")
+#
+# y = open("anotherlife.txt", "w")
+# y.write("gibberishish\n")
+#
+# z = open("anotherlife.txt", "a")
+# z.write("what if i start speaking comprehensive sentences\n")
+#
+# q = open("newestfile.txt", "w")
+# q.write("look another new file\n")
 
-y = open("anotherfile.txt", "w")
-y.write("gibberish")
-
-y = open("anotherlife.txt", "w")
-y.write("gibberishish\n")
-
-z = open("anotherlife.txt", "a")
-z.write("what if i start speaking comprehensive sentences\n")
-
-q = open("newestfile.txt", "w")
-q.write("look another new file\n")
-
-import os
+# import os
 # os.remove("anotherfile.txtl")
 # os.rmdir("gerbeg")
 
 # import module
 # module.foood()
 
-import module as snacks
-snacks.khaddodrobbo()
+# import module as snacks
+# snacks.khaddodrobbo()
+
+# from module import foood
+# foood()
